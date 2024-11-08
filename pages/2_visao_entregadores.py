511d2aa6dee487a9a5f3c94055d4280b45e306ee
Alexandre Marques da Silva <111449395+amsilva1971@users.noreply.github.com>
2024-11-08 14:47:28 -0300

    Update 2_visao_entregadores.py

--- pages/2_visao_entregadores.py
+++ pages/2_visao_entregadores.py
@@ -8,7 +8,7 @@ import plotly.express as px
 import folium
 from streamlit_folium import folium_static
 from datetime import datetime
-import altair as alt
+
 
 
 st.set_page_config (page_title='Visão entregadores', page_icon='', layout='wide') 
