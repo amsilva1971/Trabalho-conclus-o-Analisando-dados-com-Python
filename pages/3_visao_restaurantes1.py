673226dc21b2cb8ab4fdc5dd3abb6ad45838928d
Alexandre Marques da Silva <111449395+amsilva1971@users.noreply.github.com>
2024-11-11 14:48:37 -0300

    Update 3_visao_restaurantes1.py

--- pages/3_visao_restaurantes1.py
+++ pages/3_visao_restaurantes1.py
@@ -5,7 +5,7 @@ import numpy as np
 import streamlit as st
 from PIL import Image
 #from haversine import haversine
-import plotly.express as px
+#import plotly.express as px
 import plotly.graph_objects as go
 import folium
 from streamlit_folium import folium_static
