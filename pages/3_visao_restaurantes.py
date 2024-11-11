bb5e6d9ff479ef0c25cfd9ad48f798f5f45a1f7a
Alexandre Marques da Silva <111449395+amsilva1971@users.noreply.github.com>
2024-11-11 13:58:37 -0300

    Update 3_visao_restaurantes.py

--- pages/3_visao_restaurantes.py
+++ pages/3_visao_restaurantes.py
@@ -10,8 +10,7 @@ import plotly.graph_objects as go
 import folium
 from streamlit_folium import folium_static
 from datetime import datetime
-import altair as alt
-from altair.vegalite.v4.api import Chart
+
 
 st.set_page_config (page_title='Visão restaurantes', page_icon='', layout='wide') 
 
